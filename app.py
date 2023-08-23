f796e686da9dcdfed52ab774021a660f106b2a24
emsibt <tranthanhbao0309@gmail.com>
2023-08-23 09:14:09 +0700

    update cloudformation

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "<h1 style='text-align: center;'>Hello World, my name is MinhDo." \
-           " Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
+    return (
+        "<h1 style='text-align: center;'>Hello World, my name is BaoTran."
+        " Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
+    )
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)  # specify with port=80
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)  # specify with port=80
