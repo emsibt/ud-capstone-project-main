e0be701b2db3c50308dd72df1afa351228d9b2d1
emsibt <tranthanhbao0309@gmail.com>
2023-08-23 13:03:09 +0700

    add fail lint step

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print(
+        "<h1 style='text-align: center;'>Hello World, my name is baott. Welcome to my Cloud DevOps Engineer Capstone Project!</h1> "
+    )
+    return
     return (
         "<h1 style='text-align: center;'>Hello World, my name is BaoTran."
         " Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
